047f472e87c3132465dc7e28b70a31334497c182
Cup-of-Cocoa <Cup-of-Cocoa@users.noreply.github.com>
2017-01-15 18:51:24 +0900

    added

---
+++ numberPlace/MiniDiagnoalNanpureSolver.java
@@ -0,0 +1,50 @@
+package numberPlace;
+
+public class MiniDiagnoalNanpureSolver extends NanpureSolver{
+	
+	public MiniDiagnoalNanpureSolver(String[] board){
+		super(board);
+	}
+	
+	public boolean isPlaceableInDiag1(int n, int p) {
+		for (int i = 0; i < size; i++) {
+			if(board[i*size+i] == n) return false;
+		}
+		return true;
+	}
+	
+	public boolean isPlaceableInDiag2(int n, int p) {
+		for (int i = 0; i < size; i++) {
+			if(board[(size-i)*size - (size-i)] == n) return false;
+		}
+		return true;
+	}
+	
+	public boolean isPlaceableInBlock(int n, int p) {
+		int spnum;
+		spnum = (((p/6)/2)*2)*6+((p%6)/3)*3;
+		for (int i = spnum; i < spnum+3; i++) {
+			if(board[i] == n) return false;
+			if(board[i+size] == n) return false;
+		}
+		if (p%7 == 0) return isPlaceableInDiag1(n,p);
+		else if (p%5 ==0) return isPlaceableInDiag2(n,p);
+		else return true;
+	}
+	
+	public void outputBoard() {
+		for(int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				System.out.print(board[i*size+j] + " ");
+				if(j%3 == 2) System.out.print(" ");
+			}
+			System.out.println();
+			if(i%2 == 1) System.out.println(" ");
+		}
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		MiniDiagnoalNanpureSolver n = new MiniDiagnoalNanpureSolver(args);
+		n.solve();
+	}
+}
---
+++ numberPlace/NanpureSolver.java
@@ -0,0 +1,83 @@
+package numberPlace;
+
+import java.util.ArrayList;
+
+public abstract class NanpureSolver {
+	int[] board; //盤面
+	int size; //一辺のマス数
+	ArrayList<Integer> remain = new ArrayList<Integer>();//空いてるマスのリスト
+		
+	public NanpureSolver(){}
+	
+	public NanpureSolver(String[] board){
+		size = (int)Math.sqrt(board.length);
+		this.board = new int[size*size];
+		for (int i = 0; i < size*size; i++) {
+			try {
+				this.board[i] = Integer.parseInt(board[i]);
+			}
+			catch(NumberFormatException e) {
+				System.out.println("invalid input: not integer");
+			}
+			if (this.board[i] == 0) remain.add(i);
+		}
+	}
+	public boolean boardIsFull() {//盤面が埋まっているかどうか
+		return remain.isEmpty();	
+	}
+	
+	public abstract boolean isPlaceableInBlock(int n, int p);
+	
+	public boolean isPlaceable(int n, int p) {
+		if(board[p] != 0) return false;
+		for (int i = 0; i < size; i++) {
+			if(board[(p/size)*size+i] == n) return false; 
+			if(board[i*size+p%size] == n) return false;
+		}
+		return isPlaceableInBlock(n,p);		
+	}
+	
+	public void solve(){
+		if(!boardIsFull()) {
+			for(int n = 1; n <= size; n++){
+				int i = remain.get(0);
+		        if(isPlaceable(n, i)) {
+		            board[remain.get(0)] = n;
+		            remain.remove(0);
+		            solve();     
+		            board[i] = 0; 
+		            remain.add(0,i);
+		        }
+		    }
+			
+		}
+		else {
+			outputBoard();
+		}
+		
+	}
+	
+	public void outputBoard() {
+		for(int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				System.out.print(board[i*size+j] + " ");
+				if(j%3 == 2) System.out.print(" ");
+			}
+			System.out.println();
+			if(i%3 == 2) System.out.println();
+		}
+		System.out.println();
+	}
+	
+	public void outputMiniBoard() {
+		for(int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				System.out.print(board[i*size+j] + " ");
+				if(j%3 == 2) System.out.print(" ");
+			}
+			System.out.println();
+			if(i%3 == 2) System.out.println();
+		}
+		System.out.println();
+	}
+}
