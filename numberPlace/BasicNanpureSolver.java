3417669a1a50c650ad8fdfd190fe2c6a526b752c
Cup-of-Cocoa <Cup-of-Cocoa@users.noreply.github.com>
2017-01-16 18:44:33 +0900

    added

---
+++ numberPlace/BasicNanpureSolver.java
@@ -0,0 +1,25 @@
+package numberPlace;
+
+public class BasicNanpureSolver extends NanpureSolver{
+	
+	public BasicNanpureSolver(String[] board){
+		super(board);
+	}
+	
+	public boolean isPlaceableInBlock(int n, int p) {
+		int spnum;
+		spnum = (((p/9)/3)*3)*9+((p%9)/3)*3;
+		for (int i = spnum; i < spnum+3; i++) {
+			if(board[i] == n) return false;
+			if(board[i+size] == n) return false;
+			if(board[i+size*2] == n) return false;			
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		BasicNanpureSolver n = new BasicNanpureSolver(args);
+		n.solve();
+
+	}
+}
