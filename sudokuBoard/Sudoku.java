320ceca877628f99c22581208a3813db3c58c0cb
Cup-of-Cocoa <Cup-of-Cocoa@users.noreply.github.com>
2017-05-20 23:30:15 +0900

    細かい名称変更

--- sudokuBoard/Sudoku.java
+++ sudokuBoard/Sudoku.java
@@ -25,7 +25,7 @@ public class Sudoku extends JFrame implements ActionListener{
 	static final int MINI_SIZE = 6, BASIC_SIZE = 9, INITIAL_SELECTED_BOX = 0;
 	static final int BASIC_MODE = 0, MINI_MODE = 1;
 	static final String TITLE = "Number Place", MENU_NAME = "Board";
-	static final String BASIC_BOARD_MENU = "Basic", MINI_BOARD_NAME = "MINI";
+	static final String BASIC_BOARD_MENU = "Basic", MINI_BOARD_NAME = "Mini";
 	static final String BASIC_BOARD_AC = "basic", MINI_BOARD_AC = "mini";
 	static final String EMPTY_BOX = "";
 	private SudokuBoard basicBoard = new SudokuBoard(BASIC_SIZE);
